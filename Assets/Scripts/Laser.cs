57baf87d2233c67f55d835b63963ad6ea0fbb7e4
Combatd <combatdstar@gmail.com>
2025-06-17 08:17:57 -0700

    Update Laser to destroy at location, Define Laser#destroyTime

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -4,6 +4,9 @@ public class Laser : MonoBehaviour
 {
   // speed variable of 8
   private int _speed = 8;
+  // time variable of 0.5
+  private int _time = 15;
+
   // Start is called once before the first execution of Update after the MonoBehaviour is created
   void Start()
   {
@@ -16,5 +19,26 @@ public class Laser : MonoBehaviour
     // translate laser upward
     // transform.Translate(Vector3(0, 1, 0), _speed * Time.deltaTime);
     transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+    // if the laser position is greater than 8 on the y-axis
+    if (transform.position.y > 8f)
+    {
+      // if the parent is not null
+      if (transform.parent != null)
+      {
+        // destroy the parent
+        Destroy(transform.parent.gameObject);
+      }
+      // destroy the laser
+      Destroy(gameObject);
+    }
+
+    destroyTime();
+  }
+
+  void destroyTime()
+  {
+    // destroy the laser after some time
+    Destroy(gameObject, _time);
   }
 }
